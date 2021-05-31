ca4d7ac440d5054f3bb15cd628a2a1a742cc71da
borjasalom <borjasalom.dev@gmail.com>
2021-05-31 20:09:50 +0200

    Create About page

---
+++ src/components/SectionInfo.vue
@@ -0,0 +1,43 @@
+<template>
+  <section class="section-info">
+    <h3 > {{ title }}</h3>
+    <p> {{ description }}</p>
+  </section>
+</template>
+
+<script>
+
+  export default {
+    name: 'SectionChart',
+    props: {
+        title: String,
+        description: String
+    },
+  }
+</script>
+
+<style scoped>
+.section-info {
+    width: 90%;
+    padding: 20px 10px;
+    margin: 50px;
+
+    background-color: white;
+  
+    display: flex;
+    flex-flow: column;
+    align-content: center;
+    justify-content: center;
+  }
+
+  @media (min-width: 1008px) {
+    .section-info {
+      max-width: 1024px;
+      padding: 50px 50px;
+    }
+
+    .section-info h3{
+      margin-bottom: 50px;
+    }
+  }
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,32 @@
 <template>
-  <div class="about">
-    <h1>This is about</h1>
+  <div class="main">
+    <SectionInfo 
+      :title='title'
+      :description='description'
+    />
   </div>
 </template>
 
+<script>
+  import SectionInfo from '@/components/SectionInfo.vue'
+
+  export default {
+    name: 'Topics', 
+    data: function(){
+      return {
+        title: '¿Cuales son los objetivos de la Aplicación?',
+        description: "El proyecto tiene el objetivo de facilitar a los usuarios datos oficiales acerca de las \
+        preguntas escritas que realizan los diputados al gobierno dentro del parlamento. Por ende, se ha creado una aplicación \
+        web que consume la API (api.tipiciudadano.es/) para recopilar toda la información, procesarla y exponérsela a los usuarios en forma de gráfica. \
+        Cada gráfica tiene la finalidad de responder una pregunta como: ¿Sobre que temáticas suelen preguntar más los diputados? ¿Sobre que temáticas suelen \
+        preguntar más cierto grupo parlamentario? ¿Hay algunas temáticas que suelen ser más preguntadas por los diputados más jóvenes?, etc. Los datos se agrupan en dos \
+        grandes conjuntos llamados temáticas y grupos parlamentarios. Dentro de cada uno se muestra toda la información que atañe a cierta temática o grupo parlamentario. \
+        Con esta labor se pretende que el ciudadano se interese por la actividad parlamentaría y que disponga de una fuente de información fiable que no se vea influenciada por ideologías o \
+        intereses que puedan repercutir en la opinión del usuario."
+      }
+    },
+    components:{
+      SectionInfo,
+    }
+  }
+</script>
